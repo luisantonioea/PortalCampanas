3396a23977df56d5beaa07ecf7f02c48d00631d5
kurobih <lantonioespinoza10@gmail.com>
2026-03-28 13:47:09 -0500

    comentario: mejorar layout y ordenar lista

--- Controllers/CampanasController.cs
+++ Controllers/CampanasController.cs
@@ -6,9 +6,14 @@ namespace PortalCampanas.Controllers
     public class CampanasController : Controller
     {
         public IActionResult Index()
-        {
-            var campanas = CampanaRepository.Campanas;
-            return View(campanas);
-        }
+{
+    // Modificamos esta misma zona para que choque directamente con los Filtros
+    var campanasOrdenadas = CampanaRepository.Campanas.OrderBy(c => c.Nombre).ToList();
+    
+    // Un mensaje de prueba para el layout
+    ViewBag.MensajeLayout = "Layout Mejorado Activo";
+    
+    return View(campanasOrdenadas);
+}
     }
 }
