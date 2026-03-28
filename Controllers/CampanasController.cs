dde34ee9bbb5a4be425b9ed2626da0035535717a
kurobih <lantonioespinoza10@gmail.com>
2026-03-28 12:50:02 -0500

    comentario: agregar detalle de campaña

--- Controllers/CampanasController.cs
+++ Controllers/CampanasController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PortalCampanas.Data;
+using System.Linq;
 
 namespace PortalCampanas.Controllers
 {
@@ -9,6 +10,19 @@ namespace PortalCampanas.Controllers
         {
             var campanas = CampanaRepository.Campanas;
             return View(campanas);
+        }
+
+        // NUEVA ACCIÓN: Busca la campaña por su ID en la lista en memoria
+        public IActionResult Detalle(int id)
+        {
+            var campana = CampanaRepository.Campanas.FirstOrDefault(c => c.Id == id);
+            
+            if (campana == null)
+            {
+                return NotFound(); // Retorna error 404 si el ID no existe
+            }
+
+            return View(campana);
         }
     }
 }
